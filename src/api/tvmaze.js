3eccf58cbf2fc8fa845929eb982e74350ab35acd
abhay9013 <abhayporwal2303@@gmail.com>
2023-09-23 19:37:12 +0530

    fetch and display data from tvmaze api

---
+++ src/api/tvmaze.js
@@ -0,0 +1,9 @@
+const BASE_URL="https://api.tvmaze.com";
+
+const apiget = async (querystr)=>{
+    const resp = await fetch(`${BASE_URL}${querystr}`);
+    const body = await resp.json();
+    return body;
+};
+
+export const searchShows=(query)=>apiget(`/search/shows?q=${query}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+import { searchShows } from './../api/tvmaze';
+
 const Home = () => {
-  return <div>HOME PAGE</div>;
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setapiDataError] = useState(null);
+
+  const onSearchIpChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+    try {
+      setapiDataError(null);
+      const result = await searchShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      setapiDataError(error);
+    }
+  };
+
+  const renderApidata = () => {
+    if (apiDataError) {
+      return <div>Error Occured : {apiDataError.message}</div>;
+    }
+
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchIpChange} />{' '}
+        {/*value here specify two way data binding*/}
+        <button type="submit">Search</button>
+      </form>
+      <div>{renderApidata()}</div>
+    </div>
+  );
 };
 
 export default Home;
