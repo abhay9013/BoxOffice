ec2dc2e28274baab9fa2e486b8d5bebaf66e6e88
abhay9013 <abhayporwal2303@@gmail.com>
2023-09-24 17:04:59 +0530

    created show page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 import MainLayout from './components/MainLayout';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/starred" element={<Starred />} />
         </Route>
+        <Route path="/show/:showId" element={<Show />} />
         <Route path="*" element={<div>NOT FOUND</div>} />
       </Routes>
     </BrowserRouter>
