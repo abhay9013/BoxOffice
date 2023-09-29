27dc4375e9ce208dc42955102ce7d3d561e5bbde
abhay9013 <abhayporwal2303@@gmail.com>
2023-09-30 00:59:28 +0530

    fetched shows from api from api on starred page

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -10,3 +10,11 @@ export const searchShows = query => apiget(`/search/shows?q=${query}`);
 export const searchForPeople = query => apiget(`/search/people?q=${query}`);
 export const getShowById = showid =>
   apiget(`/shows/${showid}?embed[]=seasons&embed[]=cast`);
+
+export const getShowByIds = async showIds => {
+  const promises = showIds.map(showId => apiget(`/shows/${showId}`));
+
+  const result = await Promise.all(promises);
+
+  return result;
+};
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,4 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getShowByIds } from '../api/tvmaze';
+import { useStarredShows } from '../lib/useStarredShows';
+
+import ShowGrid from '../components/shows/ShowGrid';
+
 const Starred = () => {
-  return <div>Starred page</div>;
+  const [starredShowsID] = useStarredShows();
+
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['show', starredShowsID], //ressemble like dep arr in useeffect , if something here changes than it refetched
+    queryFn: () =>
+      getShowByIds(starredShowsID).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+
+  if (starredShows && starredShows.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+
+  if (starredShows && starredShows.length === 0) {
+    return <div>No show were starred</div>;
+  }
+
+  if (starredShowsError) {
+    return <div>Error occured:{starredShowsError.message}</div>;
+  }
+
+  return <div>Loading Shows..</div>;
 };
 export default Starred;
